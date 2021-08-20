63c75be689bdc047553369f80bc2a6fb903006db
guswn2521 <guswn2521@naver.com>
2021-08-21 02:36:25 +0900

    test change2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>헬로!!!!!!!!!!!!!!!!!!!!!!!!!</Text>
+      <Text>헬로!!!!!@@@!!!!!!!!!!!!!!!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
