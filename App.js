5a7a3612edacfc52dddc273bebb0da48869220d3
guswn2521 <guswn2521@naver.com>
2021-08-21 20:02:24 +0900

    #0.5 How does React Native Work? 앱을 만드는 방법 1. 완전 native : swift나 objective-c로 ios앱 만드는 것, java나 코틀린으로 안드로이드 앱 만드는 것. 2. 웹뷰기반 앱을 만드는 것. - 하이브리드, html, css를 앱안에 넣는것 *리액트 네이티브는 자바스크립트와 스마트폰의 core를 연결해주는 브릿지역할
    react native규칙
    1.view태그(div태그와 유사)와 text 태그(span태그) 안에 써야함.
    2. style은 StyleSheet.creat()로 style객체를 따로 만들어야 함.
    안에서 클래스처럼 이름을 주고 style을 줌
    App()에 적용할 태그에 style={styles.이름} 을 줘서 스타일을 매칭함.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>헬로</Text>
+      <Text style={styles.text}>헬로 어게인</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'yellowgreen',
+    
   },
+  text:{
+    color:'white',
+    fontSize: 20
+  }
 });
