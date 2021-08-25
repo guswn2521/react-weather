99a41c51066deba529e38400dd72d304b975e8fe
guswn2521 <guswn2521@naver.com>
2021-08-26 00:54:43 +0900

    #1.1 Loading Screen 1. Loading.js 파일 생성   1) StyleSheet, Text, View 모두 import해줘야함.   2) export default function Loading을 생성 : 노랑 백그라운드에 글씨 작성 - style 줌
    2. App.js
      1) Loading 함수 import : import Loading from "./Loading";
      2) export default function App 함수에 Loading 태그
        export default function App() {
          return (
            <Loading />
          );
        }

--- App.js
+++ App.js
@@ -1,48 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Loading from "./Loading"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text style={styles.text}>Hello</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text style={styles.text}>Hello</Text>
-      </View>
-      <View style={styles.yellowView}>
-        <Text style={styles.text}>Hello</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <Loading/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-
-    
-  },
-  yellowView:{
-    flex:1,
-    backgroundColor:'yellow',
-    alignItems:'center',
-    justifyContent:'center',
-    
-  },
-  blueView:{
-    flex:8,
-    backgroundColor:'blue',
-    alignItems:'center',
-    justifyContent:'center',
-    
-  },
-  text:{
-    color:'white',
-    fontSize:20
-  }
-});
