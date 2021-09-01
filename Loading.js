9d25d718a7ef8bdf33482b75aa2813e7ad3247d3
guswn2521 <guswn2521@naver.com>
2021-09-01 22:19:59 +0900

    #1.4 Getting the Weather

--- Loading.js
+++ Loading.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Loading(){
+    
     return (
     <View style={styles.container}>
         <Text style={styles.text}>Getting the fucking weather</Text>
