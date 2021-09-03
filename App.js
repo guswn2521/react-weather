5b63ce183af3fc8eebc3f569982eca0717ef6927
guswn2521 <guswn2521@naver.com>
2021-09-04 02:03:06 +0900

    #2.1 Getting the condition names 1 App.js 1) API로 변수 가져옴 -> weather에 전달 ES6 이용해서 간단하게 표현
    2. Weather.js
    1) Proptypes로 weather의 condition에 변수 조건추가

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = '5fd2370e380366fcdab429fe4c4fa8cc';
 
@@ -15,20 +16,24 @@ export default class extends React.Component {
     isLoading: true
   };
   getWeather = async(latitude, longitude) => {
-    const {data} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    const {
+      data: {
+        main : {temp},
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
-    
+    this.setState({isLoading:false , condition:weather[0].main, temp});
   };
   getLocation = async () => {
     try {
-      await Location.requestForegroundPermissionsAsync();
+      await Location.requestBackgroundPermissionsAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
+     
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -37,7 +42,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;
   }
 }
