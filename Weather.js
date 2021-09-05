013d4d6ee1dbc5c754e8952f24223f367a8e8bf1
guswn2521 <guswn2521@naver.com>
2021-09-05 22:40:25 +0900

    #2.3 Background Gradient 1. expo install expo-linear-gradient LinearGradient 태그 1) style을 container로 변경. 2) Weather 함수에 condition변수 받아옴. colors = {weatherOptions[condition].gradient}로 줌 3) MaterialCommunityIcons태그의 name도 weatherOptions에서 가져옴.
    2. "react-native"에서 StatusBar import
    -> 상태바 색깔 선택가능
    Loading과 Weather 둘 다 상태바 색깔 설정
    
    3. weatherOptions 객체 생성 : 각 날씨별 iconName, gradient설정.
    https://uigradients.com/

--- Weather.js
+++ Weather.js
@@ -1,27 +1,78 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import {MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({temp}){
+
+const weatherOptions = {
+    Thunderstorm: {
+        iconName: "weather-lightning-rainy",
+        gradient: [
+            "#03001e",
+            "#7303c0",
+            "#ec38bc",
+            "#fdeff9",]
+    },
+    Drizzle: {
+        iconName: "weather-partly-rainy",
+        gradient: ["#007991", "#78ffd6"]
+    },
+    Rain: {
+        iconName: "weather-pouring",
+        gradient: ["#9CECFB","#65C7F7","#0052D4"]
+    },
+    Snow: {
+        iconName: "weather-snowy-heavy",
+        gradient: ["#E0EAFC","#CFDEF3"]
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#22c1c3","#fdbb2d"]
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#feac5e","#c779d0","#4bc0c8"]
+    },
+    Haze: {
+        iconName: "weather-hazy",
+        gradient: ["#F8CDDA", "#1D2B64"]
+    },
+    Mist: {
+        iconName: "weather-fog",
+        gradient: ["#134e5e","#71b280"]
+    },
+    Dust: {
+        iconName: "weather-windy",
+        gradient: ["#3d7eaa","#ffe47a"]
+    }
+};
+
+export default function Weather({temp, condition}){
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >
+            <StatusBar barStyle="light-content"></StatusBar>
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy"/>
+                <MaterialCommunityIcons 
+                size={96} 
+                name={weatherOptions[condition].iconName}
+                color="white"/>
                 
                 <Text style={styles.temp}>{temp}℃</Text>
-                
             </View>
             <View style={styles.halfContainer}>
-                
             </View>
-        </View>
+        </LinearGradient>
+        
     );
 }
 
 Weather.propTypes = {
     temp:PropTypes.number.isRequired,
-    condition: PropTypes.oneOf([["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear","Clouds","Haze","Mist","Dust"]]).isRequired
+    condition: PropTypes.oneOf([["Thunderstorm", "Drizzle", "Rain", "Snow", "Clear","Clouds","Haze","Mist","Dust"]]).isRequired
 };
 
 const styles = StyleSheet.create({
@@ -30,9 +81,9 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         alignItems:"center",
     },
-    
     temp:{
-        fontSize:36
+        fontSize:36,
+        color:"white"
     },
     halfContainer:{
         flex:1,
