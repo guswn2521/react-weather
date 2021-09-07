6351a069c38a289ecca70222243c38c81316e582
guswn2521 <guswn2521@naver.com>
2021-09-08 01:30:23 +0900

    #2.4 Titles and Subtitles 1. <Text>태그로 title과 subtitle 생성 값을 weatherOptions[condition].title / subtitle로 줌.
    2. styles 값을 여러개 줄때 {{ ...styles.스타일변수, ...styles.스타일변수1 }} 로 줌
    -> 웹에서는 덮어쓰는 것 같다.

--- Weather.js
+++ Weather.js
@@ -12,39 +12,57 @@ const weatherOptions = {
             "#03001e",
             "#7303c0",
             "#ec38bc",
-            "#fdeff9",]
+            "#fdeff9",],
+        title:"Thunderstorm",
+        subtitle:"천둥번개!"
     },
     Drizzle: {
         iconName: "weather-partly-rainy",
-        gradient: ["#007991", "#78ffd6"]
+        gradient: ["#007991", "#78ffd6"],
+        title:"Drizzle",
+        subtitle:'이슬비내려'
     },
     Rain: {
         iconName: "weather-pouring",
-        gradient: ["#9CECFB","#65C7F7","#0052D4"]
+        gradient: ["#9CECFB","#65C7F7","#0052D4"],
+        title:"Rain",
+        subtitle:"비내리는 오늘밤"
     },
     Snow: {
         iconName: "weather-snowy-heavy",
-        gradient: ["#E0EAFC","#CFDEF3"]
+        gradient: ["#E0EAFC","#1D2B64"],
+        title:"Snow",
+        subtitle:"눈이 와요!"
     },
     Clear: {
         iconName: "weather-sunny",
-        gradient: ["#22c1c3","#fdbb2d"]
+        gradient: ["#22c1c3","#fdbb2d"],
+        title:"Clear",
+        subtitle:"맑음. 쨍쨍함."
     },
     Clouds: {
         iconName: "weather-cloudy",
-        gradient: ["#feac5e","#c779d0","#4bc0c8"]
+        gradient: ["#feac5e","#c779d0","#4bc0c8"],
+        title: "Cloud",
+        subtitle: "no sunshine its grey"
     },
     Haze: {
         iconName: "weather-hazy",
-        gradient: ["#F8CDDA", "#1D2B64"]
+        gradient: ["#F8CDDA", "#1D2B64"],
+        title: "Haze",
+        subtitle: "A little bit of fog with strong sunshine"
     },
     Mist: {
         iconName: "weather-fog",
-        gradient: ["#134e5e","#71b280"]
+        gradient: ["#134e5e","#71b280"],
+        title:"Mist",
+        subtitle:"촉촉한 날씨, 화장이 잘먹어요"
     },
     Dust: {
         iconName: "weather-windy",
-        gradient: ["#3d7eaa","#ffe47a"]
+        gradient: ["#3d7eaa","#ffe47a"],
+        title:"Dust",
+        subtitle:"마스크필수, 황사가 왔어요"
     }
 };
 
@@ -64,6 +82,10 @@ export default function Weather({temp, condition}){
                 <Text style={styles.temp}>{temp}℃</Text>
             </View>
             <View style={styles.halfContainer}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+                </View>
             </View>
         </LinearGradient>
         
@@ -89,5 +111,22 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:"center",
         alignItems:"center"
+    },
+    title:{
+        color:"white",
+        fontSize:44,
+        fontWeight:"300",
+        marginBottom:10
+
+    },
+    subtitle:{
+        color:"white",
+        fontWeight:"600",
+        fontSize:24
+    },
+    textContainer:{
+       alignItems:"flex-start",
+       paddingHorizontal:10
+        
     }
 })
